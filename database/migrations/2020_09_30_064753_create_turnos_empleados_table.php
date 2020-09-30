0ce18bb56b9e09f8a5b2c46596280b7497bc15d6
JuanJo961 <jauregui-94@hotmail.com>
2020-09-30 04:05:41 -0500

    Prueba

--- database/migrations/2020_09_30_064753_create_turnos_empleados_table.php
+++ database/migrations/2020_09_30_064753_create_turnos_empleados_table.php
@@ -4,21 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTurnosEmpleadosTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
+
     public function up()
-    {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-    }
+{
+    Schema::create('turnos_empleados', function (Blueprint $table) {
+        $table->bigIncrements('id');
+        $table->string('nombre', 80);
+    });
+}
 
     /**
      * Reverse the migrations.
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('turnos_empleados');
     }
 }
