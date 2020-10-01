d3517cfb54630f797057b802aac5c94f690d81e9
JuanJo961 <jauregui-94@hotmail.com>
2020-09-30 23:21:28 -0500

    migraciones muchos a muchos

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -5,19 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Categoria extends Model
+class Rol extends Model
 {
     use HasFactory;
-    public function usuario()
-    {
-        return $this->belongsTo('App\Models\Usuario');
+    public function categories(){
+        return $this->belongsToMany('\App\Categoria','roles_categorias_usuarios')
+            ->withPivot('categorias_id','status');
     }
     public function usuarios(){
         return $this->belongsToMany('\App\Usuario','roles_categorias_usuarios')
             ->withPivot('usuario_id','status');
-    }
-    public function roles(){
-        return $this->belongsToMany('\App\Rol','roles_categorias_usuarios')
-            ->withPivot('roles_id','status');
     }
 }
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Turno;
+use App\Categoria;
 
 class Usuario extends Model
 {
@@ -11,10 +13,19 @@ class Usuario extends Model
     public function categorias()
     {
         return $this->hasMany('App\Models\Categoria');
-        
+
     }
     public function turnos()
     {
         return $this->hasMany('App\Models\Turno');
+    }
+    public function roles(){
+        return $this->belongsToMany('\App\Rol','roles_categorias_usuarios')
+            ->withPivot('roles_id','status');
+    }
+
+    public function categories(){
+        return $this->belongsToMany('\App\Categoria','roles_categorias_usuarios')
+            ->withPivot('categorias_id','status');
     }
 }
